8df268fc36c1d3781c488a59f824360feb327539
deekret <55876075+deekret@users.noreply.github.com>
2020-09-30 17:23:38 +0200

    first commit

---
+++ header/Grid.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <vector>
+#include "VectorAttributes.h"
+#include "ScalarAttributes.h"
+#include <stdio.h>
+#include <string>
+#include <Eigen/Dense>
+
+using namespace std;
+
+
+class Grid
+{
+public:
+	Grid(int P, int C): scalars(P), pointNormals(P), faceNormals(C), pointsZ(P)															//Resize the array for the points and cells for the grid
+	{
+		pointsX.resize(P);
+		pointsY.resize(P);
+		pointsZ.resize(P);
+		cells.resize(3 * C);
+	}
+
+	float calculateSurfaceArea();
+
+	float calculateVolume();
+
+
+	int numPoints()
+	{
+		return pointsX.size();
+	}
+
+	int numCells()
+	{
+		return cells.size() / 3;
+	}
+
+	void getPoint(int i, float* p);
+
+	void setPoint(int i, vector<float> p);
+
+	void setCell(int cell, vector<int> vertices);
+
+	void setClass(string newcls);
+
+	string getClass();
+
+	int	 getCell(int cell, int* vertices);
+
+	int findCell(float* p);
+
+	void normalize();
+
+	void computeFaceNormals();
+
+	void computeVertexNormals();
+
+	void computeCovarianceMatrix();
+
+	void computeEigenvectors();
+
+	VectorAttributes& getFaceNormals()
+	{
+		return faceNormals;
+	}
+
+	VectorAttributes& getPointNormals()
+	{
+		return pointNormals;
+	}
+
+	Eigen::Matrix3f& getCovarianceMatrix() {
+		return covarianceMatrix;
+	}
+
+	vector<vector<float>> getEigenvectors() {
+		vector<vector<float>> vectors;
+		vectors.push_back(eigenVec1);
+		vectors.push_back(eigenVec2);
+		vectors.push_back(eigenVec3);
+
+		return vectors;
+	}
+
+	vector<Point3d> getCellCentroids();
+
+	void momentTest();
+
+	int sgn(float x) {
+		if (x > 0) return 1;
+		if (x < 0) return -1;
+		return 0;
+	}
+	void PCARotation();
+
+
+protected:
+
+	ScalarAttributes	scalars;
+
+	Eigen::Matrix3f		covarianceMatrix;
+
+	vector<float>		eigenVec1, eigenVec2, eigenVec3;
+
+	vector<Point3d>		cellCentroids;
+
+	vector<float>		pointsX, pointsY, pointsZ;
+	vector<int>			cells;
+	VectorAttributes    pointNormals;
+	VectorAttributes    faceNormals;
+	std::string	cls;
+
+	float				surfaceArea;
+	float				volume;
+	float				compactness;
+	float				boundingBoxVolume;
+	float				diameter;
+	float				eccentricity;
+
+};
+
+
+
---
+++ src/Application.cpp
@@ -0,0 +1,158 @@
+//#include <GL/glew.h>
+//#include <GL/glut.h>
+#include <GLFW/glfw3.h>
+#include <filesystem>
+#include <iostream>
+#include <io.h>
+#include <filesystem>
+
+#include <fstream>
+#include <filesystem>
+#include <tuple>
+#include <stdio.h>
+#include <string>
+#include "..\header\Grid.h"
+#include "..\header\FilterItem.h"
+#include "..\header\OFFReader.h"
+using namespace std;
+namespace fs = std::filesystem;
+void loadFilter();
+
+string fileName;
+
+int drawing_style = 0;
+const int FILTER_SIZE = 250;
+
+
+Grid* grid = 0;
+//Renderer renderer;
+
+FilterItem* fis;
+
+int index = 0;
+
+void mkdir(const char* dir)
+{
+    size_t size = strlen(dir) + 1;
+    wchar_t* ndb = new wchar_t[size];
+    size_t out;
+    mbstowcs_s(&out, ndb, size, dir, size - 1);
+    _wmkdir(ndb);
+    delete[] ndb;
+}
+
+void scanFolder(string location)
+{
+    string folder;
+    int i = 0;
+    for (const auto& entry : fs::directory_iterator(location))
+    {
+        folder = entry.path().string();
+        cout << folder << endl;
+        for (const auto& fl : fs::directory_iterator(folder + "/"))
+        {
+            string file = fl.path().string();
+            string suffix = ".off";
+            if (!(file.size() >= suffix.size() && 0 == file.compare(file.size() - suffix.size(), suffix.size(), suffix)))
+                continue;
+            cout << file << endl;
+            FilterItem fi = scanFile(file);
+            int a = folder.find("/");
+            if (a <= folder.size())
+                fi.cls = folder.substr(folder.find("/") + 1);
+            else
+                fi.cls = folder.substr(folder.find("\\") + 1);
+            fis[i] = fi;
+            i++;
+        }
+    }
+    cout << "Scanning complete!" << endl;
+}
+
+
+void keyboard(unsigned char c, int, int)					//Callback for keyboard events:
+{
+    switch (c)
+    {
+    case ' ':											    // space:   Toggle through the various drawing styles of the mesh renderer
+    {
+        break;
+    }
+    /*case 'o':
+    {
+        cout << "Please enter a name for the file" << endl;
+        string fileName;
+        cin >> fileName;
+        outputFilter(fileName);
+        break;
+    }*/
+    case 's':
+    {
+        cout << "Please enter a folder to scan" << endl;
+        string fileName;
+        cin >> fileName;
+        scanFolder(fileName);
+        break;
+    }
+    case 'l':
+    {
+        cout << "Loading from output file" << endl;
+        loadFilter();
+        break;
+    }
+    }
+}
+
+void loadFilter()
+{
+    fstream filtin;
+    string line;
+    filtin.open("FilterOutput_after.csv", ios::in);
+    if (filtin)
+    {
+        getline(filtin, line);
+        getline(filtin, line);
+        while (!filtin.eof())
+        {
+            getline(filtin, line);
+            vector<string> vec = split(line, ',');
+            if (size(vec) == 0)
+                continue;
+            FilterItem fi;
+            fi.cls = vec[1];
+            fi.numFaces = stoi(vec[2]);
+            fi.numVertices = stoi(vec[3]);
+            fi.typeOfFace = vec[4];
+            fi.minX = stof(vec[5]);
+            fi.maxX = stof(vec[6]);
+            fi.minY = stof(vec[7]);
+            fi.maxY = stof(vec[8]);
+            fi.minZ = stof(vec[9]);
+            fi.maxZ = stof(vec[10]);
+            fi.bX = stof(vec[11]);
+            fi.bY = stof(vec[12]);
+            fi.bZ = stof(vec[13]);
+            fi.path = vec[14];
+            fis[stoi(vec[0])] = fi;
+        }
+        filtin.close();
+    }
+    else
+    {
+        std::cout << "No previous filter output found" << endl;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    string input;
+    cout << "Please specify the file you want to view:" << endl;
+    cin >> input;
+
+    std::tuple<Grid*, FilterItem> tup = openFile(input);
+    grid = std::get<0>(tup);
+
+    grid->calculateVolume();
+
+    return 0;
+}
