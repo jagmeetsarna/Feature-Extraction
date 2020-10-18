47a1bb5801967e157e8403873483a43a9e2a3b53
deekret <55876075+deekret@users.noreply.github.com>
2020-10-18 19:30:20 +0200

    added the matching for the normalized features

--- src/Application.cpp
+++ src/Application.cpp
@@ -22,12 +22,19 @@ void loadFilter();
 
 string fileName;
 
-vector<tuple<string, string, vector<float>>> feature_vectors;
+vector<pair<string, vector<float>>> feature_vectors;
 
 int drawing_style = 0;
 const int FILTER_SIZE = 250;
 
 int N = 1000000;                            // Number of computations per feature
+
+
+float D1min = FLT_MAX, D2min = FLT_MAX, D3min = FLT_MAX, D4min = FLT_MAX, A3min = FLT_MAX, SAmin = FLT_MAX, COmin = FLT_MAX, BBVmin = FLT_MAX, DIAmin = FLT_MAX, ECCmin = FLT_MAX;
+float D1max = FLT_MIN, D2max = FLT_MIN, D3max = FLT_MIN, D4max = FLT_MIN, A3max = FLT_MIN, SAmax = FLT_MIN, COmax = FLT_MIN, BBVmax = FLT_MIN, DIAmax = FLT_MIN, ECCmax = FLT_MIN;
+float SAavg = 0, COavg = 0, BBVavg = 0, DIAavg = 0, ECCavg = 0, SAsd = 0, COsd = 0, BBVsd = 0, DIAsd = 0, ECCsd = 0;
+int bins = 14;
+
 
 
 Grid* grid = 0;
@@ -47,7 +54,7 @@ void mkdir(const char* dir)
     delete[] ndb;
 }
 
-bool sortbysec(const pair<pair<string, string>, float>& a, const pair<pair<string, string>, float>& b)
+bool sortbysec(const pair<string, float>& a, const pair<string, float>& b)
 {
     return (a.second < b.second);
 }
@@ -172,10 +179,48 @@ void loadDB(string file)
 
     vector<string> row;
     vector<float> features;
-    tuple<string, string, vector<float>> tup;
     string line, word, temp, name, shape_class;
     float val;
+
     getline(fin, line);
+
+    row.clear();
+    getline(fin, line);
+    stringstream s(line);
+    while (getline(s, word, ';')) {
+        row.push_back(word);
+    }
+
+    SAmin = stof(row[0]);
+    SAmax = stof(row[1]);
+    COmin = stof(row[2]);
+    COmax = stof(row[3]);
+    BBVmin = stof(row[4]);
+    BBVmax = stof(row[5]);
+    DIAmin = stof(row[6]);
+    DIAmax = stof(row[7]);
+    ECCmin = stof(row[8]);
+    ECCmax = stof(row[9]);
+
+    row.clear();
+    s.clear();
+    getline(fin, line);
+    stringstream s2(line);
+    while (getline(s2, word, ';')) {
+        row.push_back(word);
+    }
+
+    D1min = stof(row[0]);
+    D1max = stof(row[1]);
+    D2min = stof(row[2]);
+    D2max = stof(row[3]);
+    D3min = stof(row[4]);
+    D3max = stof(row[5]);
+    D4min = stof(row[6]);
+    D4max = stof(row[7]);
+    A3min = stof(row[8]);
+    A3max = stof(row[9]);
+
     getline(fin, line);
 
     while (getline(fin, line)) 
@@ -195,8 +240,8 @@ void loadDB(string file)
             features.push_back(stof(row[i]));
         }
 
-        tuple tup = make_tuple(name, shape_class, features);
-        feature_vectors.push_back(tup);
+        pair<string, vector<float>> p = make_pair(name, features);
+        feature_vectors.push_back(p);
     }
 }
 
@@ -246,9 +291,6 @@ void featureExtractNormalized(string input)
 {
     vector<Grid*> grids;
     vector<string> names;
-    int bins = 14;
-    float D1min = FLT_MAX, D2min = FLT_MAX, D3min = FLT_MAX, D4min = FLT_MAX, A3min = FLT_MAX, SAmin = FLT_MAX, COmin = FLT_MAX, BBVmin = FLT_MAX, DIAmin = FLT_MAX, ECCmin = FLT_MAX;
-    float D1max = FLT_MIN, D2max = FLT_MIN, D3max = FLT_MIN, D4max = FLT_MIN, A3max = FLT_MIN, SAmax = FLT_MIN, COmax = FLT_MIN, BBVmax = FLT_MIN, DIAmax = FLT_MIN, ECCmax = FLT_MIN;
 
     for (const auto& entry : fs::directory_iterator(input))
     {
@@ -337,6 +379,8 @@ void featureExtractNormalized(string input)
     fstream filtout;
     filtout.open("outputNorm.csv", ios::out);
     filtout << "sep=;" << endl;
+    filtout << SAmin << ";" << SAmax << ";" << COmin << ";" << COmax << ";" << BBVmin << ";" << BBVmax << ";" << DIAmin << ";" << DIAmax << ";" << ECCmin << ";" << ECCmax << endl;
+    filtout << D1min << ";" << D1max << ";" << D2min << ";" << D2max << ";" << D3min << ";" << D3max << ";" << D4min << ";" << D4max << ";" << A3min << ";" << A3max << endl;
     filtout << "name;Surface Area;Compactness;Bounding Box Volume;Diameter;Eccentricity;D1-1;D1-2;D1-3;D1-4;D1-5;D1-6;D1-7;D1-8;D1-9;D1-10;D1-11;D1-12;D1-13;D1-14;D2-1;D2-2;D2-3;D2-4;D2-5;D2-6;D2-7;D2-8;D2-9;D2-10;D2-11;D2-12;D2-13;D2-14;D3-1;D3-2;D3-3;D3-4;D3-5;D3-6;D3-7;D3-8;D3-9;D3-10;D3-11;D3-12;D3-13;D3-14;D4-1;D4-2;D4-3;D4-4;D4-5;D4-6;D4-7;D4-8;D4-9;D4-10;D4-11;D4-12;D4-13;D4-14;A3-1;A3-2;A3-3;A3-4;A3-5;A3-6;A3-7;A3-8;A3-9;A3-10;A3-11;A3-12;A3-13;A3-14" << endl;
 
     for (int i = 0; i < grids.size(); i++)
@@ -380,7 +424,7 @@ void featureExtractNormalized(string input)
     }
     filtout.close();
 
-    loadDB("outputNorm.csv");
+    //loadDB("outputNorm.csv");
 }
 
 void featureExtractStandardized(string input)
@@ -388,9 +432,6 @@ void featureExtractStandardized(string input)
     vector<Grid*> grids;
     vector<string> names;
     vector<float> SAs, COs, BBVs, DIAs, ECCs;
-    int bins = 14;
-    float D1min = FLT_MAX, D2min = FLT_MAX, D3min = FLT_MAX, D4min = FLT_MAX, A3min = FLT_MAX;
-    float D1max = FLT_MIN, D2max = FLT_MIN, D3max = FLT_MIN, D4max = FLT_MIN, A3max = FLT_MIN;
 
     for (const auto& entry : fs::directory_iterator(input))
     {
@@ -452,7 +493,6 @@ void featureExtractStandardized(string input)
         }
     }
 
-    float SAavg = 0, COavg = 0, BBVavg = 0, DIAavg = 0, ECCavg = 0, SAsd = 0, COsd = 0, BBVsd = 0, DIAsd = 0, ECCsd = 0;
     for (int i = 0; i < grids.size(); i++)
     {
         SAavg += SAs[i];
@@ -548,21 +588,28 @@ float cosineSimilarity(vector<float> s1, vector<float> s2)
 
 void startNewQuery() {
 
+
+
     cout << "Please specify the query file::" << endl;
     string file_name;
     cin >> file_name;
 
-    vector<float> query_vector;
+    int numF = feature_vectors.size();
+
+    vector<float> query_vector_f;
+    vector<float> query_vector_h;
 
     tuple tup = openFile(file_name);
     Grid* query_grid = get<0>(tup);
 
-    float s = query_grid->calculateSurfaceArea();
-    float v = query_grid->calculateVolume();
-    float r = query_grid->calculateSphericity();
-    float b = query_grid->calculateBoundingBoxVol();
-    float d = query_grid->calculateDiameter();
-    float e = query_grid->calculateEccentricity();
+    float s = normalize(query_grid->calculateSurfaceArea(), SAmin, SAmax);
+    //float v = query_grid->calculateVolume();
+    float r = normalize(query_grid->calculateSphericity(), COmin, COmax);
+    float b = normalize(query_grid->calculateBoundingBoxVol(), BBVmin, BBVmax);
+    float d = normalize(query_grid->calculateDiameter(), DIAmin, DIAmax);
+    float e = normalize(query_grid->calculateEccentricity(), ECCmin, ECCmax);
+
+
 
     query_grid->calculateD1();
     query_grid->calculateD2(N);
@@ -570,51 +617,58 @@ void startNewQuery() {
     query_grid->calculateD4(N);
     query_grid->calculateA3(N);
 
-    /*query_vector.push_back(s);
-    query_vector.push_back(v);
-    query_vector.push_back(query_grid->compactness);
-    query_vector.push_back(r);
-    query_vector.push_back(b);
-    query_vector.push_back(d);
-    query_vector.push_back(e);*/
+    vector<float> D1hist = query_grid->getD1hist(D1min, D1max, bins);
+    vector<float> D2hist = query_grid->getD2hist(D2min, D2max, bins);
+    vector<float> D3hist = query_grid->getD3hist(D3min, D3max, bins);
+    vector<float> D4hist = query_grid->getD4hist(D4min, D4max, bins);
+    vector<float> A3hist = query_grid->getA3hist(A3min, A3max, bins);
 
-    for (int i = 0; i < size(query_grid->D1hist); i++) {
-        query_vector.push_back(query_grid->D1hist[i]);
+    query_vector_f.push_back(s);
+    // query_vector.push_back(v);
+    //query_vector.push_back(query_grid->compactness);
+    query_vector_f.push_back(r);
+    query_vector_f.push_back(b);
+    query_vector_f.push_back(d);
+    query_vector_f.push_back(e);
+
+    for (int i = 0; i < bins; i++) {
+        query_vector_h.push_back(D1hist[i]);
     }
     for (int i = 0; i < size(query_grid->D2hist); i++) {
-        query_vector.push_back(query_grid->D2hist[i]);
+        query_vector_h.push_back(D2hist[i]);
     }
-    for (int i = 0; i < size(query_grid->D3hist); i++) {
-        query_vector.push_back(query_grid->D3hist[i]);
+    for (int i = 0; i < bins; i++) {
+        query_vector_h.push_back(D3hist[i]);
     }
-    for (int i = 0; i < size(query_grid->D4hist); i++) {
-        query_vector.push_back(query_grid->D4hist[i]);
+    for (int i = 0; i < bins; i++) {
+        query_vector_h.push_back(D4hist[i]);
     }
-    for (int i = 0; i < size(query_grid->A3hist); i++) {
-        query_vector.push_back(query_grid->A3hist[i]);
+    for (int i = 0; i < bins; i++) {
+        query_vector_h.push_back(A3hist[i]);
     }
 
-    vector<pair<pair<string, string>, float>> result;
-    float distance;
-    int numF = feature_vectors.size();
+    vector<pair<string, float>> result;
+    float distance = 0;
 
     for (int i = 0; i < numF; i++) {
+        float distance = 0;
 
         cout << i << endl;
 
-        vector<float> vec1 = get<2>(feature_vectors[i]);
-        //distance = eucleanDist(vec1, query_vector);
-        //distance = cosineSimilarity(vec1, query_vector);
-        distance = crossBinDist(vec1, query_vector);
+        vector<float> vec1 = get<1>(feature_vectors[i]);
+
+        vector<float> vecf(vec1.begin(), vec1.begin() + 5);
+        vector<float> vech(vec1.begin() + 5, vec1.end());
+        //distance += cosineSimilarity(vecf, query_vector_f);
+        //distance += eucleanDist(vecf, query_vector_f);
+        distance += crossBinDist(vech, query_vector_h);
 
         string name = get<0>(feature_vectors[i]);
-        string shape = get<1>(feature_vectors[i]);
 
-        cout << name + shape << endl;
+        cout << name << endl;
         cout << distance << endl;
 
-        pair<string, string> p1 = make_pair(name, shape);
-        pair<pair<string, string>, float> p2 = make_pair(p1, distance);
+        pair<string, float> p2 = make_pair(name, distance);
         result.push_back(p2);
     }
 
@@ -626,7 +680,7 @@ void startNewQuery() {
     cout << "#############" << endl;
     cout << "CLOSEST 5 SHAPES: " << endl;
     for (int i = 0; i < 5; i++) {
-        cout << result[i].first.first + ",  " + result[i].first.second << endl;
+        cout << result[i].first << endl;
         cout << "distance: ";
         cout << result[i].second << endl;
         cout << endl;
@@ -639,9 +693,6 @@ void startNewQuery() {
 int main(int argc, char* argv[])
 {
 
-    cout << "Loading default data base..." << endl;
-    loadDB("output.csv");
-
     cout << "Press q to start a new query." << endl;
 
     cout << "Press n to load a new data base with normalization." << endl;
@@ -651,6 +702,12 @@ int main(int argc, char* argv[])
     char input = _getch();
 
     if (input == 'q') {
+
+        string db_file;
+
+        cout << "Please specify the csv data base to load: " << endl;
+        cin >> db_file;
+        loadDB(db_file);
         startNewQuery();
     }
     else if (input == 'n') {
